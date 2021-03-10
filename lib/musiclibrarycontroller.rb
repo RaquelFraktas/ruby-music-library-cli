a108822e2dd66aa3de30b223966b352c6038e71e
github username <github email address>
2021-03-09 21:32:19 -0500

    Done.

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -51,19 +51,60 @@ class MusicLibraryController
         sorted_genre_array = Genre.all.sort_by do |genre| 
             genre.name
         end
-        # binding.pry`
-            sorted_genre_array.each do |genre|
-            puts "#{counter}. #{genre.name}"
-            counter += 1
+        # binding.pry
+        sorted_genre_array.each do |genre|
+        puts "#{counter}. #{genre.name}"
+        counter += 1
         end
     end
 
 
     def list_songs_by_artist
         puts "Please enter the name of an artist:"
-        user_input = gets 
-        artist_found = find_by_name(user_input)
-   
+        user_input = gets.chomp
+        # counter = 1
+            if artist = Artist.find_by_name(user_input)
+                artist 
+                sorted_song_names= artist.songs.sort do |a,b|
+                a.name<=>b.name
+                #you can also do this method with sort_by, sorting by |song| if you don't want to do a.name<=>b.name
+                #sorted_song_names= artist.songs.sort_by do |song|
+                # song.name
+                end
+                sorted_song_names.each.with_index(1) do |song,i|
+                puts "#{i}. #{song.name} - #{song.genre.name}"
+                    # counter += 1
+                end
+            else
+                nil
+            end
+
     end
+
+
+    def list_songs_by_genre
+        puts "Please enter the name of a genre:"
+        user_input = gets.chomp
+        if genre = Genre.find_by_name(user_input)
+           sorted_songs = genre.songs.sort_by do |song|
+                song.name
+            end
+            sorted_songs.each.with_index(1) do|song, i|
+                puts "#{i}. #{song.artist.name} - #{song.name}"
+            end
+        end
+
+    end
+
+    def play_song
+        puts "Which song number would you like to play?"
+        input= gets.strip
+        if song= Song.find_by_name(input)
+            puts "Playing #{song.name} by #{song.artist.name}"
+        end
+        
+
+    end
+
 
 end
